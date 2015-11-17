c85780540bea5174f54f8ad5dcf2e42f0a7d6de6
Brian Underwood <ml+github@semi-sentient.com>
2015-11-17 16:14:39 +0530

    More cap tweaks

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,5 +1,4 @@
 app_dir = File.expand_path("../../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 
@@ -9,12 +8,12 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
+listen "#{app_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/tmp/pids/unicorn.pid"
+pid "#{app_dir}/tmp/pids/unicorn.pid"
 
