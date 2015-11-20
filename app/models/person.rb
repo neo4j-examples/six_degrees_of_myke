1a568376eb53d379bf3331fb279f693b8f351a19
Brian Underwood <ml+github@semi-sentient.com>
2015-11-20 17:46:40 +0530

    Order cached name list by number of epsiodes appeared in so that common names in network appear at the top

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,8 +7,12 @@ class Person
   has_many :in, :authored_episodes, type: :HAS_AUTHOR, model_class: :Episode
   has_many :out, :mentioned_in, type: :MENTIONED_IN, model_class: :Episode
 
+  has_many :both, :episodes, type: false, model_class: :Episode
+
   def self.names_matching(name)
-    @@names ||= Person.all.pluck(:name)
+    @@names ||= Person.as(:person).episodes.query_as(:episode)
+      .with('person.name AS name', 'count(episode) AS count').order('count DESC')
+      .pluck(:name)
 
     regexp = Regexp.new(Regexp.escape(name.downcase), 'i')
 
