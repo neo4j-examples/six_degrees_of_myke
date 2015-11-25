907616bfafce4905456d915d7e7c00c9eb7db23e
Brian Underwood <ml+github@semi-sentient.com>
2015-11-25 18:00:17 +0530

    Use disk cache so that we don't chew up memory

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,13 +9,18 @@ class Person
 
   has_many :both, :episodes, type: false, model_class: :Episode
 
+  NAME_CACHE_PATH = Rails.root.join('tmp/cache/person_names')
+  NAME_CACHE = ActiveSupport::Cache::FileStore.new(NAME_CACHE_PATH, expires_in: 1.week)
+
   def self.names_matching(name)
-    @@names ||= Person.as(:person).episodes.query_as(:episode)
-      .with('person.name AS name', 'count(episode) AS count').order('count DESC')
-      .pluck(:name)
+    names = NAME_CACHE.fetch('all_names') do
+      Person.as(:person).episodes.query_as(:episode)
+        .with('person.name AS name', 'count(episode) AS count').order('count DESC')
+        .pluck(:name)
+    end
 
     regexp = Regexp.new(Regexp.escape(name.downcase), 'i')
 
-    @@names.grep(regexp)
+    names.grep(regexp)
   end
 end
